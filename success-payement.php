133ec6d748c969fcf4bb3efcaac1310fbe517cf6
Guillaume Broder <iamnotheoneyouseek@gmail.com>
2016-07-24 21:57:29 +0200

    :palm_tree: Final commit !
    Signed-off-by: Guillaume Broder <iamnotheoneyouseek@gmail.com>

--- success-payement.php
+++ success-payement.php
@@ -8,8 +8,8 @@
 				<span class="card-title">Félicitations !</span>
 				<p>Votre commande a bien était validée et est en cours de traitement.</p><br>
 				<p>Vous allez recevoir prochainement un mail recapitulatif de votre commande.</p><br>
-				<a href="./mannequin.php" class="waves-effect waves-light btn green">Voir mon mannequin</a>
-				<a href="./index.php" class="waves-effect waves-light btn green">Voir la collection</a>
+				<a href="./index.php" class="waves-effect waves-light btn green">Suivre ma commande</a>
+				<a href="./index.php" class="waves-effect waves-light btn green">Imprimer le reçu</a>
 				<a href="./index.php" class="waves-effect waves-light btn white grey-text text-darken-3">Aller à l'Accueil</a>
 			</div>
 		</div>
